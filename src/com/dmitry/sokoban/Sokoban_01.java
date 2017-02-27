562be519ef3820e95ff0f0e12b902a8433020eb1
Dmitry Pavlovsky <dmitry.pavlovsky@gmail.com>
2017-02-27 20:10:22 +0300

    local changes

--- src/com/dmitry/sokoban/Sokoban_01.java
+++ src/com/dmitry/sokoban/Sokoban_01.java
@@ -5,7 +5,6 @@ public class Sokoban_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("f");
-		System.out.println("h");
-		System.out.println("g");
+		
 	}
 }
